82407d3b1f079b665044bf646674ae7cb2a54c8c
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-05-07 13:40:55 -0400

    update code format

--- setup.py
+++ setup.py
@@ -2,30 +2,29 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup(
-        name='mmschema',
+        name="mmschema",
         version="1",
-        description='A schema for classical & molecular mechanics',
-        author='',
+        description="A schema for classical & molecular mechanics",
+        author="",
         url="https://github.com/MolSSI/mmschema",
-        license='',
+        license="",
         packages=setuptools.find_packages(),
         install_requires=[
-            'jsonschema',
+            "jsonschema",
         ],
         extras_require={
-            'docs': [
-                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
-                'sphinxcontrib-napoleon',
-                'sphinx_rtd_theme',
-                'numpydoc',
+            "docs": [
+                "sphinx==1.2.3",  # autodoc was broken in 1.3.1
+                "sphinxcontrib-napoleon",
+                "sphinx_rtd_theme",
+                "numpydoc",
             ],
-            'tests': [
-                'pytest',
+            "tests": [
+                "pytest",
             ],
         },
-
         tests_require=[
-            'pytest',
+            "pytest",
         ],
         zip_safe=True,
     )
