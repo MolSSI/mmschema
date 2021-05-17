4f5b664a0cbe254e5f74376e952f8a6e246ab981
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-05-17 15:14:03 -0400

    use gen_rst from writer.py

--- docs/source/gen_schema_docs.py
+++ docs/source/gen_schema_docs.py
@@ -1,7 +1,6 @@
 """
 Very hacky way to write out the schema (for demo purposes only)
 """
-# Import headers from this
 import sys
 import os
 
@@ -9,21 +8,26 @@ sys.path.insert(1, os.path.dirname(__file__))
 import schema_doc_helpers as sh
 import mmschema.dev
 
-def gen_rst(model_name):
+def gen_rst(model_name, mode="w", filename=None):
 
     model = getattr(mmschema.dev, model_name)
 
     mfile = [f"{model_name} Schema"]
-    mfile.append("=" * len(mfile[-1]))
+
+    if mode == "w":
+        mfile.append("=" * len(mfile[-1]))
+    else:
+        mfile.append("^" * len(mfile[-1]))
 
     mfile.extend(
         f"""A full description of the overall {model_name} model.""".splitlines()
     )
 
+
     props = model["properties"]
     req = model["required"]
 
-    sh.write_header(mfile, "Required Keys")
+    sh.write_subsubsection(mfile, "Required Keys")
 
     mfile.extend(
         f"""The following properties are required for the {model_name} specification.""".splitlines()
@@ -32,7 +36,7 @@ def gen_rst(model_name):
     sh.write_key_table(mfile, props, req)
 
     ### Optional properties
-    sh.write_header(mfile, "Optional Keys")
+    sh.write_subsubsection(mfile, "Optional Keys")
 
     mfile.extend(
         f"""The following keys are optional for the {model_name} specification.""".splitlines()
@@ -40,6 +44,9 @@ def gen_rst(model_name):
 
     sh.write_key_table(mfile, props, set(props.keys()) - set(req))
 
+    mfile.extend("\n\n")
+
     # Write out the file
-    with open(f"auto_{model_name.lower()}.rst", "w") as outfile:
+    filename = filename or f"auto_{model_name.lower()}.rst"
+    with open(filename, mode) as outfile:
         outfile.write("\n".join(mfile))
