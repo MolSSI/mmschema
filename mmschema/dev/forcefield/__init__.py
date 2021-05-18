7eaf658b956e6c7e63fcb3bb8026688fbfc37b23
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-05-18 16:16:26 -0400

    define __all__

--- mmschema/dev/forcefield/__init__.py
+++ mmschema/dev/forcefield/__init__.py
@@ -1,3 +1,9 @@
-from .forcefield_base import ForceField
-from .bonds import Bonds
-from .nonbonded import NonBonded
+from .forcefield_base import *
+from .bonds import *
+from .nonbonded import *
+
+from . import forcefield_base
+from . import bonds
+from . import nonbonded
+
+__all__ = forcefield_base.__all__ + bonds.__all__ + nonbonded.__all__
