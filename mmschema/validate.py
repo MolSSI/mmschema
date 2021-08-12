fedf965838e9e79ca2affe7ba05bba995fa4f73b
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-08-12 13:53:03 -0400

    improve numpy array handling

--- mmschema/validate.py
+++ mmschema/validate.py
@@ -4,12 +4,17 @@ Schema validation tools
 
 import jsonschema
 from . import versions
+from typing import Dict, Any
 
 
-def validate(data, schema_type, version="dev"):
+def validate(data: Dict[str, Any], schema_type: str, version: int):
     """
     Validates a given input for a schema input and output type.
     """
     schema = versions.get_schema(schema_type, version)
+
+    for key, value in data.items():
+        if hasattr(value, "shape"):
+            data[key] = value.tolist()  # expensive
 
     jsonschema.validate(data, schema)
